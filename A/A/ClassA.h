ab8e93c47a41c4659868a6f0b06ce24feb2520d9
weiguang <liweiguang@duia.com>
2019-07-04 18:08:59 +0800

    add ClassA

---
+++ A/A/ClassA.h
@@ -0,0 +1,17 @@
+//
+//  ClassA.h
+//  A
+//
+//  Created by weiguang on 2019/7/4.
+//  Copyright © 2019 duia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClassA : NSObject
+- (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A/A/ClassA.m
@@ -0,0 +1,15 @@
+//
+//  ClassA.m
+//  A
+//
+//  Created by weiguang on 2019/7/4.
+//  Copyright © 2019 duia. All rights reserved.
+//
+
+#import "ClassA.h"
+
+@implementation ClassA
+- (void)test {
+    NSLog(@"hhahhha");
+}
+@end
